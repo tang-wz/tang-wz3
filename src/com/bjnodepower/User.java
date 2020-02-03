38382277a4afc9c1c8fc1b4833b9e58224da223d
tang-w-z <1334185341@qq.com>
2020-02-03 17:31:22 +0800

    123

---
+++ src/com/bjnodepower/User.java
@@ -0,0 +1,23 @@
+package com.bjnodepower;
+
+public class User {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    private String userName;
+}
